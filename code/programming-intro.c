4c59efa753161d3b6440a877d1568e46b6334b39
Joe Loy <josephl@schulerprogram.org>
2022-08-23 13:17:36 -0500

    fix c programming demo

--- code/programming-intro.c
+++ code/programming-intro.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main() {
 	int addResult;
@@ -15,7 +16,8 @@ int main() {
 
 	int x = 7;
 	int y = 4;
-
+    printf("x is %d\n", x);
+    printf("y is %d\n", y);
 	addResult = x + y;
 	subtractResult = x - y;
 	multiplyResult = x * y;
@@ -27,32 +29,41 @@ int main() {
 	incrementResult = ++x;
 	decrementResult = --y;
 
-	printf("The result is %d\n", addResult);
-	printf("Operator demo complete.\n");
+	printf("The result is x + y is %d\n", addResult);
+    printf("The result is x - y is %d\n", subtractResult);
+    printf("The result is x * y is %d\n", multiplyResult);
+    printf("The result is x / y is %d\n", divideResult);
+    printf("The result is x > y is %d\n", greaterThanResult);
+    printf("The result is x < y is %d\n", lessThanResult);
+    printf("The result is x == y is %d\n", equalsResult);
+    printf("The result is x != y is %d\n", doesNotEqualResult);
+    printf("The result is ++x is %d\n", incrementResult);
+    printf("The result is --y is %d\n", decrementResult);
+	printf("Operator demo complete.\n\n");
 
 	// example if, else if, else statement in c
-	int x = 1;
-	int y = 0;
+	x = 1;
+	y = 0;
 	if (x > y) {
   		printf("x is greater than y\n");
 	} else if(y > x) {
-  		printf(y is greater than x\n");
+  		printf("y is greater than x\n");
 	} else {
   		printf("x is equal to y\n");
 	}
-	printf("Conditional logic demo is done running\n");
+	printf("Conditional logic demo is done running\n\n");
 
 	// example for statement in C
 	for (int x = 0; x < 3; x++) {
-  		printf("x is %d", x);
+  		printf("x is %d\n", x);
 	}
 	
-	printf("For loop demo is done running.\n");
+	printf("For loop demo is done running.\n\n");
 
 	// example while statement in C
-	int x = 5;
+	x = 5;
 	while (x > 0) {
-  		printf("x is %d", x);
+  		printf("x is %d\n", x);
   		x--;
 	}
 	
